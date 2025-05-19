b74c9c33e28cf34e1279d6d1f681135aef5f7db4
NNA Design <nabilahatiqah12@gmail.com>
2025-05-20 02:25:28 +0800

    Update ucapan.js

--- ucapan.js
+++ ucapan.js
@@ -1,5 +1,1 @@
-const ucapan = [
-  { nama: "Ahmad & Aisyah", mesej: "Tahniah! Semoga berbahagia hingga ke Jannah ❤️" },
-  { nama: "Zul & Lina", mesej: "Selamat pengantin baru! Moga kekal hingga akhir hayat." },
-  { nama: "Farah", mesej: "Semoga diberkati dan dikurniakan zuriat yang soleh dan solehah." }
-];
+
